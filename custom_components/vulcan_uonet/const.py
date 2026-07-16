76835303e0e4a7aa3f37efffcd1472cff8a211e9
lukkic1205 <68222699+lukkic1205@users.noreply.github.com>
2026-07-16 14:20:04 +0200

    Update const.py

--- custom_components/vulcan_uonet/const.py
+++ custom_components/vulcan_uonet/const.py
@@ -1,1 +1,11 @@
+"""Stałe integracji Vulcan UONET+."""
 
+DOMAIN = "vulcan_uonet"
+
+CONF_TOKEN = "token"
+CONF_SYMBOL = "symbol"
+CONF_PIN = "pin"
+CONF_DEVICE_MODEL = "device_model"
+
+DEFAULT_SYMBOL = "gdansk"
+DEFAULT_DEVICE_MODEL = "Home Assistant"
